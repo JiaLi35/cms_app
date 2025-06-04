b008dc74832b336ac55fdb0aed739b050a0443e1
Jia Li <chin.j3li@gmail.com>
2025-06-05 01:26:20 +0800

    a

--- pages/post.php
+++ pages/post.php
@@ -24,7 +24,7 @@
 
     <div class="container mx-auto my-5" style="max-width: 500px;">
       <h1 class="h1 mb-4 text-center"><?= $post["title"]; ?></h1>
-      <h3 class="mb-4">By <?= $post["name"]; ?></h1>
+      <h3 class="mb-4">By <?= $post["name"]; ?></h3>
       <div class="mb-2">
         <img src="<?= $post["image"]; ?>" class="img-fluid">
       </div>
